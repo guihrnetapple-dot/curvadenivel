825658956f1c5f081cd9f5926fdeb13902a9b7e0
guihrnetapple-dot <guihrnetapple-dot@users.noreply.github.com>
2026-06-20 23:07:24 -0300

    Importa estilos profissionais da interface

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "leaflet-draw/dist/leaflet.draw.css";
 
 import { Aplicacao } from "./App";
 import "./styles.css";
+import "./ui-pro.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
